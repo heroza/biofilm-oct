0ae3d1135275284eae0cf20265608ceadb935671
Rahmat Izwan Heroza <ewa_heroza@yahoo.com>
2024-02-26 10:33:39 +0000

    added measure_one_png

--- measure.py
+++ measure.py
@@ -5,6 +5,7 @@ from fun import *
 from OctCorrection import *
 from ImageProcessing import *
 import sys
+from PIL import Image
 
 def load_csv_images(folder_path):
     image_array = []
@@ -29,22 +30,31 @@ def load_csv_images(folder_path):
     df_paths = pd.DataFrame({'FilePath': file_paths})
     return np.array(image_array), df_paths
 
-plate = sys.argv[1]
-folder_path = "/home/rh22708/darpa/dataset/annotated/Plate "+plate+'/'
-annotated_images, df1 = load_csv_images(folder_path)
+def measure_csv_in_folder(folder_path):
+    annotated_images, df1 = load_csv_images(folder_path)
+    print("Shape of the loaded annotated images array:", annotated_images.shape)
 
-# Display the shape of the resulting array
-print("Shape of the loaded annotated images array:", annotated_images.shape)
+    thr=0    
+    small_obj=30  
+    cnames=['plate','volume','thickness','Rq', 'Ra', 'density dist.', 'pixel-based density']
+    df2 = pd.DataFrame(columns =cnames )
+    for idx, image in enumerate(annotated_images):
+        fig, V,H,Rq,Ra, SL, density=CalcZMap(image[None,:],thr,small_obj) 
+        df2=pd.concat([df2, pd.DataFrame([[plate,V,H,Rq,Ra, SL, density]], columns = cnames)])   #append volume, height and roughness results in a dataframe
+        plt.savefig(folder_path+df1.loc[idx, 'FilePath']+"_output"+'.png')
+    df2.reset_index(inplace=True, drop=True)
+    df = pd.concat([df1, df2], axis=1)
+    df.to_csv(plate+'.csv', index=False)
 
-thr=0     #set here the gray value threshold to calculate the binary mask
-small_obj=30   #set here the minimum size of objects to be kept in the binary mask
-cnames=['plate','volume','thickness','Rq', 'Ra', 'density dist.', 'pixel-based density']
-df2 = pd.DataFrame(columns =cnames )
-for idx, image in enumerate(annotated_images):
-    fig, V,H,Rq,Ra, SL, density=CalcZMap(image[None,:],thr,small_obj) 
-    df2=pd.concat([df2, pd.DataFrame([[plate,V,H,Rq,Ra, SL, density]], columns = cnames)])   #append volume, height and roughness results in a dataframe
-    plt.savefig(folder_path+df1.loc[idx, 'FilePath']+"_output"+'.png')
-df2.reset_index(inplace=True, drop=True)
-df = pd.concat([df1, df2], axis=1)
-# Save the DataFrame to a CSV file
-df.to_csv(plate+'.csv', index=False)
+# plate = sys.argv[1]
+# folder_path = "/home/rh22708/darpa/dataset/annotated/Plate "+plate+'/'
+# measure_csv_in_folder(folder_path)
+
+path = sys.argv[1]
+png_image = Image.open(path)
+image_array = np.array(png_image)
+fig, V,H,Rq,Ra, SL, density=CalcZMap(image_array[None,:]) 
+file_name = os.path.basename(path)
+directory = os.path.dirname(path)
+full_path = os.path.join(directory, file_name.replace('Mask.png', 'Line.png'))
+fig.savefig(full_path)
